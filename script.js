af0825a023825b40c546349efc8b5a9c3f0fa389
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-13 13:35:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+napis = "Akademia108"
+function sortString(str) {
+  // return str.split('').sort().join('');
+  // return str.split('').sort().join('');
+
+  return str.split('');
+}
+console.log(sortString(napis));
+
+/* 
+Napisz funkcję, która pobiera jako parametr stringa (czyli napis) i zwraca ten napis z posortowanymi literami.
+
+Następnie wywołaj funkcję i posortuj stringa "Akademia108", a wynik działania funkcji wyświetl w konsoli.
+
+Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
+
+Podpowiedź przed wysłaniem zadania do sprawdzenia:
+
+najprościej zadanie zrobić używając funkcji javascript do operacji na tablicach */
